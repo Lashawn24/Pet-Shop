1ab4c18a4349e561d9b1a118969fb9b85372c4ff
Lashawn <lashawnwood23@gmail.com>
2022-05-05 21:11:19 -0400

    update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,52 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+var shop = [
+    {
+      title: 'Tigers',
+      image: './img/gerbile.jpg',
+      price: '$7,500-15,000'
+      description
+    },
+    
+  
+    
+    ]
+    
+    var postHTML = " "
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
+  
+  
+  
+    for (var i=0; i < ; i++){
+        var heading = '<div class="product ' + '"><span><h5>' + shop[i].title + '</h5>'
+        var image = '<img src="' + shop[i] + '"/'
+        var price = '<p> $' + .price + '</p></span>'
+        var description = '<div class=""><p>'+ .description + '</p><button type="button" class="btn btn-warning"> add to cart</button></div></div>'
+        var concatThis = heading + image + price + description;
+        postHTML = postHTML + concatThis
+    }
+    document.getElementById('market').innerHTML = postHTML
+  
